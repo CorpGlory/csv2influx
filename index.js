f21aed94aeb28adae0db58076b47ad8273d557ae
rozetko <rozetko09@gmail.com>
2017-11-07 21:48:44 +0300

    Resolved conflict with master

--- index.js
+++ index.js
@@ -3,72 +3,61 @@
 const config = require('./config');
 const importer = require('./importer');
 const progress = require('progress');
+const minimist = require('minimist')
 
 const usage = `
 Usage:
-  csv2influx init                                         Creates template config file
-  csv2influx [--config path/to/config.json] data.csv      Loads config from path/to/config.json then imports file data.csv to your influx
-                                                          Default path: ./csv2influx.conf.json
+  csv2influx init                     Creates template config file
+  csv2influx [options] data.csv       Imports file data.csv to your influxDB
+
+Options:
+  -c, --config /path/to/config.json   [optional] Specifies path to your config file.
+                                      Default: ./csv2influx.conf.json
+  -q, --quiet                         [optional] Makes output quiet (progress bar instead of written to DB values)
 `
 
-switch(process.argv.length)
-{
-    case 3:
-        if(process.argv[2] === 'init') {
-          config.initConfig();
-          process.exit(0);
-        }
-        else {
-          var conf = config.loadConfig();
-          var isQuiet = process.argv.indexOf('-q') >= 0;
-          // consider process.argv[2] as csv-filename
-          var inputFile = process.argv[2];
-          var progressBar = undefined;
-        }
-        
-    case 5:
-    case 6:
-        if(process.argv[2] === '--config') {
-            // consider process.argv[3] as config-filename
-            var conf = config.loadConfig(process.argv[3]);
-            var isQuiet = process.argv.indexOf('-q') >= 0;
-            // consider process.argv[4] as csv-filename
-            var inputFile = process.argv[4];
-            var progressBar = undefined;
-            
-            break;
+var args = minimist(process.argv.slice(2), {
+  string: ['config'],
+  boolean: ['quiet'],
+  alias: { q: 'quiet', c: 'config' },
+  default: {
+    config: 'csv2influx.conf.json'
+  }
+});
+
+if(args._.length > 0) {
+  if(args._[0] === 'init') {
+    config.initConfig();
+    process.exit(0);
+  } else {
+    var conf = config.loadConfig(args.config);
+    var inputFile = args._[0];
+    var progressBar = undefined;
+
+    importer.countFileLines(inputFile)
+      .then(linesCount => {
+        console.log('lines count:' + linesCount);
+        if(args.quiet) {
+          progressBar = new progress(':current/:total (:percent) :bar ', {
+            width: 100,
+            total: linesCount
+          });
         }
 
-    default:
-        console.log(usage);
-        process.exit(0);
-}
-
-if (isQuiet) {
-  importer.countFileLines(inputFile)
-    .then(linesCount => {
-      console.log('lines count:' + linesCount);
-      progressBar = new progress(':current/:total (:percent) :bar ', { 
-        width: 100, 
-        total: linesCount
-      });
-      var imp = new importer.Importer(conf, inputFile, progressBar);
-      imp.run()
-        .then(() => console.log(''))
-        .catch(err => {
-          progressBar.terminate();
-          console.error(err);
-          process.exit(1);
-        });
-    });
+        var imp = new importer.Importer(conf, inputFile, progressBar)
+        imp.run()
+          .then(() => console.log(''))
+          .catch(err => {
+            if(progressBar) {
+              progressBar.terminate();
+            }
+            console.error(err);
+            process.exit(1);
+          });
+      })
+      .catch(err => console.error(err));
+  }
 } else {
-  var imp = new importer.Importer(conf, inputFile, progressBar);
-  imp.run()
-    .then(() => console.log(''))
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
-    });
+  console.log(usage);
+  process.exit(0);
 }
-
-
