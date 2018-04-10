b6f1dac28f5395a4d21cb8dd9122a10fd181ca06
rozetko <rozetko09@gmail.com>
2018-04-10 20:11:13 +0300

    Fix config file name bug

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ function _reformatConfigObject(confObj) {
 }
 
 function loadConfig(config_file_name) {
-  config_file_name = config_file_name === undefined ? config_file_name : INIT_CONF_FILE_NAME;
+  config_file_name = config_file_name !== undefined ? config_file_name : INIT_CONF_FILE_NAME;
 
   console.log('Reading ' + config_file_name);
   if(!fs.existsSync(config_file_name)) {
